78188eaaf634fc685c463d70b189fa6de2a4ec6f
Mariusz Perzynski <m.perzyk@interia.pl>
2021-05-02 14:08:31 +0200

    update codes

--- forex.py
+++ forex.py
@@ -1,10 +1,10 @@
 # pip install forex-python !
+
 from forex_python.converter import CurrencyRates
 
 print("..Currency Converter From Forex..")
 
-cash = CurrencyRates()   # creating instance
+cash = CurrencyRates()  
+case = cash.convert("USD","INR",1)
 
-r = cash.convert("USD","INR",1)
-
-print("Convert USD to INR =======> ",r)
+print("Convert USD to INR =======> ",case)
