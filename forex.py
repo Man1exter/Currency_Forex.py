b38aab3dc637775ac0d43cc4aee3894e3873e8c7
Mariusz Perzynski <m.perzyk@interia.pl>
2021-05-02 14:07:44 +0200

    add convert from USD to INR

--- forex.py
+++ forex.py
@@ -1,4 +1,10 @@
 # pip install forex-python !
-
+from forex_python.converter import CurrencyRates
 
 print("..Currency Converter From Forex..")
+
+cash = CurrencyRates()   # creating instance
+
+r = cash.convert("USD","INR",1)
+
+print("Convert USD to INR =======> ",r)
