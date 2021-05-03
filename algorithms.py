d30c0d44b13b6e37b2d1cc20b03c39f0f4a72efe
Mariusz Perzynski <m.perzyk@interia.pl>
2021-05-03 11:49:49 +0200

    upgrade algorithm

--- algorithms.py
+++ algorithms.py
@@ -1,23 +1,24 @@
-def Bin_Search(tab[], x, left, right):             T(n)
-    
-  if(left = rigth-1):                              0(1)
+def Bin_Search(tab, x, left, right):                #T(n)
+                                                    
+  if(left == right - 1):                            #0(1)
 
-    return 0                                       0(1)
+    return 0                                        #0(1)
 
-  mid = (left+rigth)//2                            0(1)
+  mid = (left + right) // 2                         #0(1)
  
-  if(tab[mid] = x):                                0(1)
+  if(tab[mid] == x):                                #0(1)
 
-    return mid                                     0(1)
+    return mid                                      #0(1)
 
-  if(tab[mid] > x:                                 0(1)
+  if(tab[mid] > x):                                 #0(1)
 
-    return Bin_Search(tab[], x, left, mid)         T(n/2)
+    return Bin_Search(tab, x, left, mid)            #T(n/2)
 
-  else:                                            0(1)
+  else:                                             #0(1)
 
-    return Bin_Search(tab[], x, mid, right)        T(n/2)
+    return Bin_Search(tab, x, mid, right)           #T(n/2)
 
+#T(n) = T(1/2)
+#T(n) = 0(f(n))
 
-T(n) = T(1/2)
-T(n) = 0(f(n))
+Bin_Search(1,2,3,4)
