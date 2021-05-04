4b346d5b673a968297d5fab9aceb011a604e1492
Mariusz Perzynski <m.perzyk@interia.pl>
2021-05-04 13:16:47 +0200

    add space in text with elements to use

--- forex.py
+++ forex.py
@@ -5,6 +5,6 @@ from forex_python.converter import CurrencyRates
 print("..Currency Converter From Forex..")
 
 cash = CurrencyRates()  
-case = cash.convert("USD","INR",1)
+case = cash.convert( "USD" , "INR" , 1 )
 
 print("Convert USD to INR =======> ",case)
