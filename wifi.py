977e1bd95d3ac70e0c8cbea9c4fa0693223a28bd
Mariusz Perzynski <m.perzyk@interia.pl>
2021-05-05 17:01:22 +0200

    update data

--- wifi.py
+++ wifi.py
@@ -1,3 +1,9 @@
 import subprocess
+
 print("wifi password")
 
+data = subprocess.check_output(['netsh','wlan','show','profiles']).decode('utf-8').split("\n")
+profiles = [i.split(":")[l][l:-l] for i in data if "All User Profile" in l]
+
+for i in profiles:
+    results = subprocess.check_output
